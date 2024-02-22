4c6c1dd2bd774d02c3377af3b1bf227ed0d88e3e
Irene-24 <okaforirenen@gmail.com>
2024-02-22 19:19:11 +0100

    path alias set up

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const createApp = () => {
+  const app = express();
+  return app;
+};
+
+export default createApp;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hhhh");
+// import loaders from "@/loaders";
+import createApp from "./app";
+// import config from "./config";
+
+const app = createApp();
+
+// loaders({ expressApp: app });
+
+const config = {
+  port: 4040,
+};
+
+app
+  .listen(config.port, () => {
+    console.info(`
+      #######################################################################
+          🛡️  Server is available at: http://localhost:${config.port} 🛡️
+      #######################################################################
+    `);
+  })
+  .on("error", (err) => {
+    console.error(err);
+    process.exit(1);
+  });
